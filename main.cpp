79f55dc6ab0df4ea813de2710016c2a6508db209
zhouhmg <zhouhmgz@gmail.com>
2020-10-12 15:37:12 +0800

    When dt increase, the result will be affect.

--- main.cpp
+++ main.cpp
@@ -26,14 +26,15 @@ int pid_test(){
     //假设读到的数据是-90 - 90 间，目标输出 50
     int targetVal = 50;
     srand((int)(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count()));
-    for(int x=0;x<10/*sizeof(data_array)/sizeof(int)*/;x++){
-        float rdw = rand() % (90-(-90)+1)+ (-90);//产生随机数：表示  a=90~b=-90 之间的一个随机整数。
-        float error  = rdw;
-        float control= pid.get_pid(-(error - targetVal) , 1);//补偿值
-        float control_out = rdw + control;//加上补偿值后，这个约等于targetVal
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        std::cout << x<<"read:"<< error << " target:"<< (control_out)<< " control: " << control << std::endl;
+    int data_count = 10;
+    for(int x=0;x<data_count/*sizeof(data_array)/sizeof(int)*/;x++){
+        float read_data_from_sensor = rand() % (90-(-90)+1)+ (-90);//产生随机数：表示  a=90~b=-90 之间的一个随机整数。
+        float control= pid.get_pid(-(read_data_from_sensor - targetVal) , 1);//补偿值
+        float control_out = read_data_from_sensor + control;//加上补偿值后，这个约等于targetVal
+        total+= control_out;
+        std::this_thread::sleep_for(std::chrono::milliseconds(800));
+        std::cout << x<<"read:"<< read_data_from_sensor << " target:"<< (control_out)<< " control: " << control << std::endl;
 
     }
-    std::cout << "avg:"<< total/20 << std::endl;
+    std::cout << "avg:"<< total/data_count << std::endl;
 }
